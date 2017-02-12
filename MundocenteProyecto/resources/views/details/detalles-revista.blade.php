6da7fa8f9eaff4b13562dff280a90cb6e623f0fc
Diego Sosa <diego.sosa0704@gmail.com>
2017-02-12 18:46:50 -0500

    Cambio diseño de detalles por modals en la sección inicio o publication

--- MundocenteProyecto/resources/views/details/detalles-revista.blade.php
+++ MundocenteProyecto/resources/views/details/detalles-revista.blade.php
@@ -1,14 +1,8 @@
-@extends('main.main')
-
-@section('content')
-
-
-    <!--Contenido-->
-    <div class="pusher" style="background-color: #EEEEEE;">
-        <!--Top menu fixed-->
+    <div class="ui modal details-paper">
+        <i class="close icon"></i>
         <div class="segment-title">
             <div class="ui left aligned container">
-                <h1 class="ui header">Nombre Evento</h1>
+                <h1 class="ui header">Nombre Revista</h1>
                 <div class="overlay">
                     <div class="ui secondary menu">
                         <div class="item">
@@ -27,133 +21,127 @@
                 </div>
             </div>
         </div>
-        <div class="ui container">
-            <div class="ui piled very padded segment">
-                <div class="ui equals width stackable form grid">
-                    <div class="two column row">
-                        <div class="column">
+        <div class="content">
+            <div class="ui equals width stackable form grid">
+                <div class="two column row">
+                    <div class="column">
+                        <div class="field">
+                            <label>Imagen o logo del evento</label>
                             <div class="field">
-                                <label>Imagen o logo del evento</label>
-                                <div class="field">
-                                    <img class="ui centered medium image" src="images/public-image.png" alt="">
-                                </div>
-                            </div>
-                        </div>
-                        <div class="column">
-                            <div class="field">
-                                <label>Institución editora:</label>
-                                <div class="ui large label color_2">Nombre institución</div>
-                            </div>
-                            <div class="field">
-                                <label>País:</label>
-                                <div class="ui large label color_2">Nombre del país</div>
-                            </div>
-                            <div class="field">
-                                <label>Ciudad:</label>
-                                <div class="ui large label color_1">Nombre de la Ciudad</div>
+                                <img class="ui centered medium image" src="images/public-image.png" alt="">
                             </div>
                         </div>
                     </div>
-                </div>
-                <div class="ui form">
-                    <div class="two fields">
-                    </div>
-                    <div class="field">
-                        <h5 class="ui header"><b>Áreas de conocimiento</b></h5>
-                        <div class="three fields">
-                            <div class="field">
-                                <div class="ui raised card">
-                                    <div class="content">
-                                        <div class="header">Gran Área</div>
-                                        <div class="ui celled ordered list">
-                                            <div class="item">Cats</div>
-                                            <div class="item">Horses</div>
-                                            <div class="item">Dogs</div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="field">
-                                <div class="ui raised card">
-                                    <div class="content">
-                                        <div class="header">Área</div>
-                                        <div class="ui celled ordered list">
-                                            <div class="item">Cats</div>
-                                            <div class="item">Horses</div>
-                                            <div class="item">Dogs</div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="field">
-                                <div class="ui raised card">
-                                    <div class="content">
-                                        <div class="header">Disciplina</div>
-                                        <div class="ui celled ordered list">
-                                            <div class="item">Cats</div>
-                                            <div class="item">Horses</div>
-                                            <div class="item">Dogs</div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
+                    <div class="column">
+                        <div class="field">
+                            <label>Institución editora:</label>
+                            <div class="ui large label color_2">Nombre institución</div>
                         </div>
-                    </div>
-
-                    <label><b>¿La revista se encuentra indexada?</b></label>
-                    <div class="inline field">
-                        <label>
-                            No
-                        </label>
-                        <div class="ui toggle checkbox" onclick="showAdvancedSearch()">
-                            <input type="checkbox" name="indexed_paper" tabindex="0" class="hidden"
-                                   id="checkpaperindex">
+                        <div class="field">
+                            <label>País:</label>
+                            <div class="ui large label color_2">Nombre del país</div>
                         </div>
-                        <label>
-                            Si
-                        </label>
-                    </div>
-                    <!--Datos de indexación-->
-                    <div id="indexing-data" class="ui segment" style="display: none;">
-                        <h4 class="ui dividing bold header">Datos de indexación</h4>
-                            <div class="inline field">
-                                <label>Nombre indice</label>
-                                <select name="name" class="ui fluid dropdown"
-                                        id="selectpaperindex">
-                                    <option value="">Clasficación</option>
-                                    <option value="1">Nivel</option>
-                                </select>
-                            </div>
-                    </div>
-                    <div class="field">
-                        <h5 class="ui header"><b>Detalles</b></h5>
-                        <div class="ui segment">
-                            <span><b>Datos de contacto para ampliar información: </b></span>
-                            <p>Nulla facilisi. Sed sed vehicula risus. Phasellus facilisis tellus leo, in aliquam risus
-                                euismod et.</p>
-
-
-                            <span><b>Ver convocatoria: </b></span>
-                            <a class="ui teal tag label">Enlace a convocatoria</a>
+                        <div class="field">
+                            <label>Ciudad:</label>
+                            <div class="ui large label color_1">Nombre de la Ciudad</div>
                         </div>
                     </div>
                 </div>
             </div>
+            <div class="ui form">
+                <div class="two fields">
+                </div>
+                <div class="field">
+                    <h5 class="ui header"><b>Áreas de conocimiento</b></h5>
+                    <div class="three fields">
+                        <div class="field">
+                            <div class="ui raised card">
+                                <div class="content">
+                                    <div class="header">Gran Área</div>
+                                    <div class="ui celled ordered list">
+                                        <div class="item">Cats</div>
+                                        <div class="item">Horses</div>
+                                        <div class="item">Dogs</div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="field">
+                            <div class="ui raised card">
+                                <div class="content">
+                                    <div class="header">Área</div>
+                                    <div class="ui celled ordered list">
+                                        <div class="item">Cats</div>
+                                        <div class="item">Horses</div>
+                                        <div class="item">Dogs</div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="field">
+                            <div class="ui raised card">
+                                <div class="content">
+                                    <div class="header">Disciplina</div>
+                                    <div class="ui celled ordered list">
+                                        <div class="item">Cats</div>
+                                        <div class="item">Horses</div>
+                                        <div class="item">Dogs</div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <label><b>¿La revista se encuentra indexada?</b></label>
+                <div class="inline field">
+                    <label>
+                        No
+                    </label>
+                    <div class="ui toggle checkbox" onclick="showAdvancedSearch()">
+                        <input type="checkbox" name="indexed_paper" tabindex="0" class="hidden"
+                               id="checkpaperindex">
+                    </div>
+                    <label>
+                        Si
+                    </label>
+                </div>
+                <!--Datos de indexación-->
+                <div id="indexing-data" class="ui segment" style="display: none;">
+                    <h4 class="ui dividing bold header">Datos de indexación</h4>
+                    <div class="inline field">
+                        <label>Nombre indice</label>
+                        <select name="name" class="ui fluid dropdown"
+                                id="selectpaperindex">
+                            <option value="">Clasficación</option>
+                            <option value="1">Nivel</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="field">
+                    <h5 class="ui header"><b>Detalles</b></h5>
+                    <div class="ui segment">
+                        <span><b>Datos de contacto para ampliar información: </b></span>
+                        <p>Nulla facilisi. Sed sed vehicula risus. Phasellus facilisis tellus leo, in aliquam risus
+                            euismod et.</p>
+
+
+                        <span><b>Ver convocatoria: </b></span>
+                        <a class="ui teal tag label">Enlace a convocatoria</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="actions">
+            <div class="ui button">Aceptar</div>
         </div>
     </div>
 
+    <!--Contenido-->
+
     <script type="text/javascript">
-        $('.overlay')
-            .visibility({
-                type: 'fixed',
-                offset: 70 // give some space from top of screen
-            })
-        ;
-        $('.dropdown')
-            .dropdown({
-                // you can use any ui transition
-                transition: 'fade'
-            })
+        $('.modal.details-paper')
+            .modal('show')
         ;
         function showAdvancedSearch() {
             $('#indexing-data').toggle("slow");
@@ -162,5 +150,3 @@
             .checkbox()
         ;
     </script>
-
-@stop
--- MundocenteProyecto/resources/views/details/detalles-solicitud.blade.php
+++ MundocenteProyecto/resources/views/details/detalles-solicitud.blade.php
@@ -1,11 +1,5 @@
-@extends('main.main')
-
-@section('content')
-
-
-    <!--Contenido-->
-    <div class="pusher" style="background-color: #EEEEEE;">
-        <!--Top menu fixed-->
+    <div class="ui modal details-request">
+        <i class="close icon"></i>
         <div class="segment-title">
             <div class="ui left aligned container">
                 <h1 class="ui header">Título</h1>
@@ -33,98 +27,104 @@
                 </div>
             </div>
         </div>
-        <div class="ui container">
-            <div class="ui piled very padded segment">
-                <div class="ui equals width stackable form grid">
-                    <div class="two column row">
-                        <div class="column">
-                            <div class="field">
-                                <label>Sector educativo: </label>
-                                <div class="ui large label color_1">Sectores</div>
+        <div class="content">
+            <div class="ui equals width stackable form grid">
+                <div class="two column row">
+                    <div class="column">
+                        <div class="field">
+                            <label>Sector educativo: </label>
+                            <div class="ui large label color_1">Sectores</div>
+                        </div>
+                        <div class="field">
+                            <label>Institución: </label>
+                            <div class="ui large label color_2">Nombre institución</div>
+                        </div>
+                    </div>
+                    <div class="column">
+                        <div class="field">
+                            <label>País: </label>
+                            <div class="ui large label color_2">Nombre del país</div>
+                        </div>
+                        <div class="field">
+                            <label>Ciudad:</label>
+                            <div class="ui large label color_1">Nombre de la Ciudad</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="ui form">
+                <div class="two fields">
+                </div>
+                <div class="field">
+                    <h5 class="ui header"><b>Áreas de conocimiento</b></h5>
+                    <div class="three fields">
+                        <div class="field">
+                            <div class="ui raised card">
+                                <div class="content">
+                                    <div class="header">Gran Área</div>
+                                    <div class="ui celled ordered list">
+                                        <div class="item">Cats</div>
+                                        <div class="item">Horses</div>
+                                        <div class="item">Dogs</div>
+                                    </div>
+                                </div>
                             </div>
                         </div>
-                        <div class="column">
-                            <div class="field">
-                                <label>Institución: </label>
-                                <div class="ui large label color_2">Nombre institución</div>
+                        <div class="field">
+                            <div class="ui raised card">
+                                <div class="content">
+                                    <div class="header">Área</div>
+                                    <div class="ui celled ordered list">
+                                        <div class="item">Cats</div>
+                                        <div class="item">Horses</div>
+                                        <div class="item">Dogs</div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="field">
+                            <div class="ui raised card">
+                                <div class="content">
+                                    <div class="header">Disciplina</div>
+                                    <div class="ui celled ordered list">
+                                        <div class="item">Cats</div>
+                                        <div class="item">Horses</div>
+                                        <div class="item">Dogs</div>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div class="ui form">
-                    <div class="two fields">
-                    </div>
-                    <div class="field">
-                        <h5 class="ui header"><b>Áreas de conocimiento</b></h5>
-                        <div class="three fields">
-                            <div class="field">
-                                <div class="ui raised card">
-                                    <div class="content">
-                                        <div class="header">Gran Área</div>
-                                        <div class="ui celled ordered list">
-                                            <div class="item">Cats</div>
-                                            <div class="item">Horses</div>
-                                            <div class="item">Dogs</div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="field">
-                                <div class="ui raised card">
-                                    <div class="content">
-                                        <div class="header">Área</div>
-                                        <div class="ui celled ordered list">
-                                            <div class="item">Cats</div>
-                                            <div class="item">Horses</div>
-                                            <div class="item">Dogs</div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="field">
-                                <div class="ui raised card">
-                                    <div class="content">
-                                        <div class="header">Disciplina</div>
-                                        <div class="ui celled ordered list">
-                                            <div class="item">Cats</div>
-                                            <div class="item">Horses</div>
-                                            <div class="item">Dogs</div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="field">
-                        <h5 class="ui header"><b>Detalles</b></h5>
-                        <div class="ui segment">
-                            <span><b>Descripción: </b></span>
-                            <p>In at dolor euismod lacus venenatis aliquet id id sapien. Suspendisse consequat turpis
-                                lorem,
-                                in tristique nibh vestibulum et. Sed porta massa vel purus porta suscipit. Duis pretium
-                                et
-                                arcu vel consequat. Fusce nec felis sed sem ultricies feugiat. Donec sed odio eget erat
-                                fermentum porttitor ut id dui.e</p>
-                            <span><b>Datos de contacto para ampliar información: </b></span>
-                            <p>Nulla facilisi. Sed sed vehicula risus. Phasellus facilisis tellus leo, in aliquam risus
-                                euismod et.</p>
+                <div class="field">
+                    <h5 class="ui header"><b>Detalles</b></h5>
+                    <div class="ui segment">
+                        <span><b>Descripción: </b></span>
+                        <p>In at dolor euismod lacus venenatis aliquet id id sapien. Suspendisse consequat turpis
+                            lorem,
+                            in tristique nibh vestibulum et. Sed porta massa vel purus porta suscipit. Duis pretium
+                            et
+                            arcu vel consequat. Fusce nec felis sed sem ultricies feugiat. Donec sed odio eget erat
+                            fermentum porttitor ut id dui.e</p>
+                        <span><b>Datos de contacto para ampliar información: </b></span>
+                        <p>Nulla facilisi. Sed sed vehicula risus. Phasellus facilisis tellus leo, in aliquam risus
+                            euismod et.</p>
 
 
-                            <div class="field">
-                                <label>Fecha:</label>
-                                <div class="ui celled horizontal list">
-                                    <div class="item">
+                        <div class="field">
+                            <label>Fecha:</label>
+                            <div class="ui celled horizontal list">
+                                <div class="item">
                                         <span>
                                             <b>Desde:</b>
                                             Fecha
                                         </span>
-                                    </div>
-                                    <div class="item">
+                                </div>
+                                <div class="item">
                                         <span>
                                             <b>Hasta:</b>
                                             Fecha
                                         </span>
-                                    </div>
                                 </div>
                             </div>
                         </div>
@@ -132,21 +132,14 @@
                 </div>
             </div>
         </div>
+        <div class="actions">
+            <div class="ui button">Aceptar</div>
+        </div>
     </div>
 
+
     <script type="text/javascript">
-        $('.overlay')
-            .visibility({
-                type: 'fixed',
-                offset: 70 // give some space from top of screen
-            })
-        ;
-        $('.dropdown')
-            .dropdown({
-                // you can use any ui transition
-                transition: 'fade'
-            })
+        $('.modal.details-request')
+            .modal('show')
         ;
     </script>
-
-@stop
